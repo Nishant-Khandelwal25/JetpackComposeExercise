40561442bb3e0ca8d8b82aff6c391871521323db
nishant <Nishant.khandelwal@turtlemint.com>
2023-08-25 20:07:42 +0530

    Gradient with Image and text implementation

--- app/src/main/java/com/example/jetpackcomposeexercise/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeexercise/MainActivity.kt
@@ -3,13 +3,111 @@ package com.example.jetpackcomposeexercise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            /*   Row(
+                   modifier = Modifier
+                       .fillMaxSize(0.5f)
+                       .background(Color.Green),
+                   verticalAlignment = Alignment.Bottom,
+                   horizontalArrangement = Arrangement.SpaceAround,
+               ) {
+                   Text(text = "Hello")
+                   Text(text = "World")
+               }*/
+
+            /*Column(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .background(Color.Blue),
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.End
+            ) {
+                Text(text = "Hello1")
+                Text(text = "World1")
+            }*/
+
+            val painter = painterResource(id = R.drawable.sample)
+            val contentDescription = "This is a sample image used for tutorial"
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    title = contentDescription
+                )
+            }
+
+
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter, contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
         }
     }
 }
